6e9e8ac1bdcdf4a2c03ca881bda98c7d630c9310
kimi <bbcauwbakdina@gmail.com>
2025-02-12 05:54:21 +0000

    fix

--- src/handlers/admin/__init__.py
+++ src/handlers/admin/__init__.py
@@ -1,7 +1,7 @@
-from handlers.admin.moderation_settings.moderation_handler import AdminModerationHandler
-from handlers.admin.moderation_settings.warning_handler import AdminWarningHandler
-from handlers.admin.moderation_settings.auto_action_handler import AdminAutoActionHandler
-from handlers.admin.moderation_settings.punishment_handler import AdminPunishmentHandler
+from src.handlers.admin.moderation_settings.moderation_handler import AdminModerationHandler
+from src.handlers.admin.moderation_settings.warning_handler import AdminWarningHandler
+from src.handlers.admin.moderation_settings.auto_action_handler import AdminAutoActionHandler
+from src.handlers.admin.moderation_settings.punishment_handler import AdminPunishmentHandler
 from src.handlers.admin.log_handler import AdminLogHandler
 
 __all__ = [
